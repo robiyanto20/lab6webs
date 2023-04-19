955521d3da385b347b9ba7a25a6f84c4e6525108
robiypcs <38169654+robiyanto20@users.noreply.github.com>
2023-04-19 23:04:37 +0700

    Update header.php

--- template/header.php
+++ template/header.php
@@ -5,12 +5,12 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="author" content="Akbaroke">
-  <link rel="icon" href="https://avatars.githubusercontent.com/u/94231436?v=4" />
+  <meta name="author" content="anon">
+  <link rel="icon" href="#" />
 
   <!-- css -->
   <script src="https://cdn.tailwindcss.com"></script>
-  <title>Project Praktikum 6 | Muhammad Akbar</title>
+  <title>$$$</title>
 </head>
 
 <body class="dark:bg-slate-900 dark:text-white">
